9deba388460654e36f3ece48a5c3f0187f28c336
willfs144 <willfs144@gmail.com>
2022-06-11 12:53:46 -0500

    comentario en codigo. prueba de conexion con jenkins

--- Main.java
+++ Main.java
@@ -3,5 +3,6 @@ class Main{
  	public static void main(String[] args) {
 		System.out.println("Hola Mundo, desde java");
 		System.out.println("La conexion con Github se ha hecho");
+		//comentario
 	}
 }
